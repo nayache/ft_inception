d47b427f15cd2f09c8b5a95e19efa67365cd15f0
nayache <nayache@student.42.fr>
2022-09-21 16:53:03 +0200

    maybe finish

--- mariadb/setup.sh
+++ mariadb/setup.sh
@@ -1,11 +1,14 @@
-service mysql start;
-
 if [ ! -d "/var/lib/mysql/$DB_NAME" ]
 then
-    mysql -u root < init.sql;
+    service mysql start;
+   mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+   mysql -u root -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+   mysql -u root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${MYSQL_USER}'@'%';"
+   mysql -u root -e "FLUSH PRIVILEGES;"
+   mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
+   service mysql stop;
 else
     echo "==> Database already exists";
 fi
 
-service mysql stop;
 mysqld;
--- wordpress/entrypoint.sh
+++ wordpress/entrypoint.sh
@@ -5,10 +5,15 @@ service php7.3-fpm start;
 
 if [ ! -f /var/www/html/wordpress/wp-config.php ]
 then
+    ## download wordpress
     wp core download --allow-root --path="/var/www/html/wordpress";
     chown -R www-data:www-data /var/www/*
-    wp config create --dbname=wordpress --dbuser=nayache --dbpass=pass --dbhost=mariadb --allow-root --path="/var/www/html/wordpress";
-    wp core install --url=https://nayache.42.fr --title="==>INCEPTION<==" --admin_user=nayache --admin_password=pass --admin_email=nana@gmail.com --skip-email --allow-root --path="/var/www/html/wordpress"
+    ## creation wp-config.php
+    wp config create --dbname=$DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST --allow-root --path="/var/www/html/wordpress";
+    ## installation wordpress
+    wp core install --url=https://nayache.42.fr --title="==>INCEPTION<==" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root --path="/var/www/html/wordpress";
+    ## creation second user wordpress
+    wp user create $WP_USER $WP_EMAIL --user_pass=$WP_PASSWORD --allow-root --path="/var/www/html/wordpress";
 else
     echo "==> Wordpress is already installed"
 fi
