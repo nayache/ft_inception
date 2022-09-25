6d4a03163ee21a77f0a33feeff39c0f71ce01798
nayache <nayache@student.42.fr>
2022-09-25 18:39:46 +0200

    works well

---
+++ Makefile
@@ -0,0 +1,39 @@
+
+all: volumes_dir build up
+
+build:
+	docker-compose -f ./srcs/docker-compose.yml build
+
+up:
+	docker-compose -f ./srcs/docker-compose.yml up
+
+stop:
+	docker-compose -f ./srcs/docker-compose.yml stop
+
+down:
+	docker-compose -f ./srcs/docker-compose.yml down
+
+re:
+	docker-compose -f srcs/docker-compose.yml up --build
+
+clear_volumes_dir:
+	sudo rm -rf /data/wordpress /data/mariadb;
+
+clean:
+	docker stop $$(docker ps -qa);\
+	docker rm $$(docker ps -qa);\
+	docker rmi -f $$(docker images -qa);\
+	docker volume rm $$(docker volume ls -q);\
+	docker network rm $$(docker network ls -q);
+
+fclean: clear_volumes_dir clean
+
+volumes_dir: /data/wordpress /data/mariadb
+
+/data/wordpress:
+	sudo mkdir -p /data/wordpress
+
+/data/mariadb:
+	sudo mkdir -p /data/mariadb
+
+.PHONY: all re down clean
--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-chown -R www-data:www-data /var/www/*
-service php7.3-fpm start;
 
 if [ ! -f /var/www/html/wordpress/wp-config.php ]
 then
+    chown -R www-data:www-data /var/www/*
+    service php7.3-fpm start;
     ## download wordpress
     wp core download --allow-root --path="/var/www/html/wordpress";
     chown -R www-data:www-data /var/www/*
@@ -14,9 +14,10 @@ then
     wp core install --url=https://nayache.42.fr --title="==>INCEPTION<==" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root --path="/var/www/html/wordpress";
     ## creation second user wordpress
     wp user create $WP_USER $WP_EMAIL --user_pass=$WP_PASSWORD --allow-root --path="/var/www/html/wordpress";
+    
+    service php7.3-fpm stop;
 else
     echo "==> Wordpress is already installed"
 fi
 
-service php7.3-fpm stop;
 php-fpm7.3 -F;
